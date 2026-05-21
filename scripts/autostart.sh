cebb436c8939b1ec840862a9253e1b5289878714
IT –  Grejdzher Programmer <134506062+TKakashi@users.noreply.github.com>
2026-05-21 23:01:06 +0300

    Update autostart.sh

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Скрипт автозапуска для Hyprland
 
+# Панель задач и уведомления
+waybar &
+swaync &
+
+# Управление питанием по умолчанию (сбалансированный режим)
+~/.config/hypr/scripts/power.sh balanced
